43b2a0597a3920c951772b730d279ecb734f4bf7
jobin.joseph <jobin.joseph@scrapehero.com>
2025-01-30 21:35:43 +0530

    backend user creation logic updated

--- aibf_backend/settings.py
+++ aibf_backend/settings.py
@@ -42,7 +42,27 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5174",
     "https://www.aibf.org.au",
     "https://aibf-backend-production.up.railway.app",
-    "*"
+]
+CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
 ]
 CSRF_TRUSTED_ORIGINS = ['https://aibf-backend.up.railway.app']
 
